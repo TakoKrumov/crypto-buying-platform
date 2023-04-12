5d60dbf844b9f114ae6d302237af5f48586ab6ff
Milen <petrovmilen38@gmail.com>
2023-04-12 15:33:23 +0300

    added ticker functuonality with websocket and changing colors when price goes up or down

--- src/components/TickerPrice/TickerPrice.js
+++ src/components/TickerPrice/TickerPrice.js
@@ -1,33 +1,38 @@
 // components/TickerPrice.js
 import React, { useState, useEffect } from 'react';
 import ReconnectingWebSocket from 'reconnecting-websocket';
+import './TickerPrice.scss';
 
 const TickerPrice = ({ symbol }) => {
   const [price, setPrice] = useState(null);
+  const [priceChange, setPriceChange] = useState(null);
 
   useEffect(() => {
-    // Create a WebSocket connection to the Binance API
     const ws = new ReconnectingWebSocket(
       `wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@ticker`
     );
 
-    // Listen for updates from the WebSocket
     ws.addEventListener('message', (event) => {
       const data = JSON.parse(event.data);
-      setPrice(data.c); // Update the price
+      const newPrice = parseFloat(data.c).toFixed(2);
+
+      if (price) {
+        setPriceChange(newPrice > price ? 'up' : 'down');
+      }
+
+      setPrice(newPrice);
     });
 
-    // Clean up the WebSocket connection when the component is unmounted
     return () => {
       ws.close();
     };
-  }, [symbol]);
+  }, [symbol, price]);
 
   return (
     <div>
       {price ? (
-        <p>
-          {symbol}: ${price}
+        <p className="price">
+          {symbol}: $<span className={priceChange}>{price}</span>
         </p>
       ) : (
         <p>Loading...</p>
