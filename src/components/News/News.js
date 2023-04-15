2297bb9e063c51582e366a7c00b4d6d7eddcf151
Milen <petrovmilen38@gmail.com>
2023-04-15 16:07:01 +0300

    changes to coins,navigation,routing, added global state and new api calls

---
+++ src/components/News/News.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Select,Typography,Row,Col,Avatar ,Card } from 'antd';
+import moment from 'moment';
+
+const News = () => {
+  return (
+    <div>News</div>
+  );
+};
+
+export default News;
