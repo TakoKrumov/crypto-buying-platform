6f61ff71f2904081203cbd848c3cf5c75f8b1b22
“[Tako <takokrumov@hotmail.com>
2023-04-20 10:35:11 +0300

    fixes in wallet and a bit stylling.

--- src/components/UserInfo/Wallet/Wallet.js
+++ src/components/UserInfo/Wallet/Wallet.js
@@ -1,106 +1,17 @@
-// import React, { useState, useEffect, useContext } from "react";
-// import "./Wallet.scss";
-// import { useGetCryptosQuery } from "../../../services/cryptoApi";
-// import CryptoCurrencies from "../../CryptoCurrencies/CryptoCurrencies";
-// import { AuthContext } from "../../../contexts/authContext";
-
-// const Wallet = () => {
-//   const count = 100;
-//   const { data: cryptoList } = useGetCryptosQuery(count);
-//   const [symbol, setSymbol] = useState("");
-//   const [amount, setAmount] = useState(0);
-//   const [selectedPrice, setSelectedPrice] = useState(0);
-
-//   const coinData = cryptoList?.data.coins;
-
-//   const handleCurrencyChange = (event) => {
-//     const selectedIndex = event.target.selectedIndex;
-//     const selectedCoinPrice = parseFloat(event.target.value);
-//     const selectedCoin = event.target.options[selectedIndex].id;
-//     setSelectedPrice(selectedCoinPrice);
-//     console.log(selectedCoin);
-//     setSymbol(selectedCoin);
-// };
-
-//   const handleAmountChange = (event) => {
-//     setAmount(event.target.value);
-//   };
-
-//   const handleBuyCoins = () => {
-//     const user = JSON.parse(localStorage.getItem('auth'));
-//     const coinPurchase = {
-//       symbol: symbol,
-//       amount: parseFloat(amount),
-//       pricePerCoin: selectedPrice,
-//       purchaseTotal: parseFloat(amount) * selectedPrice
-//     };
-  
-//     // Check if the user has enough funds to buy the coins
-//     if (user.portfolio.wallet[0].fundsInAccount >= coinPurchase.purchaseTotal) {
-//       // Deduct the purchase amount from the user's funds
-//       user.portfolio.wallet[0].fundsInAccount -= coinPurchase.purchaseTotal;
-  
-//       // Check if the user already has the selected coin in their wallet
-//       const existingCoin = user.portfolio.wallet[0].buyCoins.find(coin => coin.symbol === symbol);
-  
-//       if (existingCoin) {
-//         // If the coin exists, update the coin amount
-//         existingCoin.amount += parseFloat(amount);
-//       } else {
-//         // If the coin doesn't exist, add the coin and its amount to the wallet
-//         user.portfolio.wallet[0].buyCoins.push({
-//           symbol: symbol,
-//           amount: parseFloat(amount)
-//         });
-//       }
-  
-//       // Save the updated user object back to Local Storage
-//       localStorage.setItem('auth', JSON.stringify(user));
-  
-//       // Reset the form fields
-//       setSymbol('');
-//       setAmount(0);
-//       setSelectedPrice(0);
-//     } else {
-//       alert("You don't have enough funds to buy the selected coins.");
-//     }
-//   };
-  
-
-//   const handleAddFunds = () => {};
-
-//   return (
-//     <div className="wallet">
-//       <div className="wallet-left">
-//         <h1>Crypto Wallet</h1>
-//         <h1>Buying coins</h1>
-//         <label htmlFor="currency">Currency:</label>
-//         <select name="currency" onChange={handleCurrencyChange}>
-//           <option value="" id="">
-//             Select currency
-//           </option>
-//           {coinData?.map((coin, index) => (
-//             <option key={index} value={coin.price} id={coin.symbol}>
-//               {coin.symbol}
-//             </option>
-//           ))}
-//         </select>
-//         <label htmlFor="">How much coins do you like to buy?</label>
-
-
 import React, { useState, useEffect, useContext } from "react";
 import "./Wallet.scss";
+import img from "./spinner.gif";
 import { useGetCryptosQuery } from "../../../services/cryptoApi";
 import CryptoCurrencies from "../../CryptoCurrencies/CryptoCurrencies";
-import { AuthContext } from "../../../contexts/authContext";
-import { updateMe } from "../../../services/authService";
 
 const Wallet = () => {
   const count = 100;
   const { data: cryptoList } = useGetCryptosQuery(count);
   const [symbol, setSymbol] = useState("");
   const [amount, setAmount] = useState(0);
+  const [icon, setIcon] = useState(img);
   const [selectedPrice, setSelectedPrice] = useState(0);
+  const [myMoney, setMyMoney] = useState(JSON.parse(localStorage.getItem('auth')).portfolio.wallet[0].fundsInAccount)
 
   const coinData = cryptoList?.data.coins;
 
@@ -111,103 +22,144 @@ const Wallet = () => {
     setSelectedPrice(selectedCoinPrice);
     console.log(selectedCoin);
     setSymbol(selectedCoin);
+    console.log(event.target.options[selectedIndex]);
+    setIcon(event.target.options[selectedIndex].dataset.icon);
   };
 
+  // const handleAmountChange = (event) => {
+  //   setAmount(event.target.value);
+  // };
   const handleAmountChange = (event) => {
-    setAmount(event.target.value);
+    let inputAmount = parseFloat(event.target.value);
+    const maxAmount = parseFloat(myMoney / selectedPrice.toFixed(2));
+
+    if (inputAmount >= maxAmount) {
+      inputAmount = maxAmount.toFixed(2) ;
+    }
+
+    setAmount(inputAmount);
   };
 
   const handleBuyCoins = () => {
-    
     // Check if the user selected a coin symbol
     if (symbol === "") {
       alert("Please select a coin to buy.");
       return;
     }
-  
-    const user = JSON.parse(localStorage.getItem('auth'));
+
+    const user = JSON.parse(localStorage.getItem("auth"));
     const coinPurchase = {
       symbol: symbol,
       amount: parseFloat(amount).toFixed(2),
       pricePerCoin: parseFloat(selectedPrice).toFixed(2),
-      purchaseTotal: parseFloat(amount * selectedPrice).toFixed(2)
+      purchaseTotal: parseFloat(amount * selectedPrice).toFixed(2),
     };
-  
+
     // Check if the user has enough funds to buy the coins
-    if (parseFloat(user.portfolio.wallet[0].fundsInAccount) >= parseFloat(coinPurchase.purchaseTotal)) {
+    if (
+      parseFloat(user.portfolio.wallet[0].fundsInAccount) >=
+      parseFloat(coinPurchase.purchaseTotal)
+    ) {
       // Deduct the purchase amount from the user's funds
-      user.portfolio.wallet[0].fundsInAccount = (parseFloat(user.portfolio.wallet[0].fundsInAccount) - parseFloat(coinPurchase.purchaseTotal)).toFixed(2);
-  
+      user.portfolio.wallet[0].fundsInAccount = (
+        parseFloat(user.portfolio.wallet[0].fundsInAccount) -
+        parseFloat(coinPurchase.purchaseTotal)
+      ).toFixed(2);
+
       // Check if the user already has the selected coin in their wallet
-      const existingCoin = user.portfolio.wallet[0].buyCoins.find(coin => coin.symbol === symbol);
-  
+      const existingCoin = user.portfolio.wallet[0].buyCoins.find(
+        (coin) => coin.symbol === symbol
+      );
+
       if (existingCoin) {
         // If the coin exists, update the coin amount
-        existingCoin.amount = (parseFloat(existingCoin.amount) + parseFloat(amount)).toFixed(2);
+        existingCoin.amount = (
+          parseFloat(existingCoin.amount) + parseFloat(amount)
+        ).toFixed(2);
       } else {
         // If the coin doesn't exist, add the coin and its amount to the wallet
         user.portfolio.wallet[0].buyCoins.push({
           symbol: symbol,
-          amount: parseFloat(amount).toFixed(2)
+          amount: parseFloat(amount).toFixed(2),
         });
       }
-  
+
       // Save the updated user object back to Local Storage
-      localStorage.setItem('auth', JSON.stringify(user));
-  
+      localStorage.setItem("auth", JSON.stringify(user));
+
       // Reset the form fields
-      setSymbol('');
+      setSymbol("");
       setAmount(0);
       setSelectedPrice(0);
-  
+      setIcon(img)
+
       // Reset the select form
       document.querySelector("select[name='currency']").selectedIndex = 0;
     } else {
       alert("You don't have enough funds to buy the selected coins.");
     }
-    const loggedUser = JSON.parse(localStorage.getItem('auth'))
     
-
   };
-  
 
   const handleAddFunds = () => {};
 
   return (
     <div className="wallet">
       <h1>Crypto Wallet</h1>
-      <div className="wallet-left">      
+      <div className="wallet-buyingCrypto">
         <h3>Buying coins</h3>
-        <label htmlFor="currency">Currency:</label>
-        <select name="currency" onChange={handleCurrencyChange}>
-          <option value="" id="">
-            Select currency
-          </option>
-          {coinData?.map((coin, index) => (
-            <option key={index} value={coin.price} id={coin.symbol}>
-              {coin.symbol} 
+        <div className="crpExh-container">
+          <label htmlFor="currency">Currency:</label>
+          <span>
+          <select name="currency" onChange={handleCurrencyChange}>
+            
+            <option value="0" id="">
+              Select...
             </option>
-          ))}
-        </select>
-        <label htmlFor="amount">How much coins do you like to buy?</label>
+            {coinData?.map((coin, index) => (
+              <option
+                key={index}
+                value={coin.price}
+                id={coin.symbol}
+                data-icon={coin.iconUrl}
+              >
+                {coin.symbol}
+              </option>
+            ))}
+          </select>
+          <img
+            src={!icon === true ? img : icon}
+            alt=""
+            className="crpExh-icon"
+          />
+          </span>
+        </div>
+        <div className="crpExh-container">
+              <label htmlFor="pricePerCoin">Price per coin:</label>
+              <span>${selectedPrice.toFixed(2)} </span>
+        </div>
+
+        <div className="crpExh-container">
+        <label htmlFor="amount">Buying Quantity:</label>
         <input
           type="number"
           min={0}
+          max = {(myMoney/selectedPrice).toFixed(2)}
           name="amount"
-          value={parseFloat(amount)}
+          value={!!amount ? parseFloat(amount) : 0}
           onChange={handleAmountChange}
+          
         />
-        <label htmlFor="amount">
-          Coin amount: {amount} {symbol} <br />*<br /> ${selectedPrice.toFixed(2)} {symbol}
-          <br /> Total: ${(amount * selectedPrice).toFixed(2)}
-        </label>
-        
-        <button onClick={handleBuyCoins} className="crpExh-btn">Buy Coins</button>
-
+       </div>
+          <span type="radio" name="" id="crpExh-total" /> Total: ${(amount * selectedPrice).toFixed(2)}
+        <button onClick={handleBuyCoins} className="crpExh-btn">
+          Buy
+        </button>
       </div>
       <h1>Adding funds</h1>
-      <div className="wallet-right"></div>
-    </div>
+      <div className="wallet-addingFunds"></div>
+      </div>
+    
   );
 };
 
