0c2543b25ea5d4ea41ee4e131267c771746b365c
Milen <petrovmilen38@gmail.com>
2023-04-25 10:12:04 +0300

    fixed styling in the navigation bar and some color scemes for the themes

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -20,11 +20,11 @@ const Homepage = () => {
     <div className='homeContainer'>
       <Title level={2} className='heading'>Global Crypto Stats</Title>
       <Row className='light-theme'>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col>
-        <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
-        <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/></Col>
-        <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
-        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" className='statistic-title' value={globalStats.total}/></Col>
+        <Col span={12}><Statistic title="Total Exchanges" className='statistic-title' value={millify(globalStats.totalExchanges)}/></Col>
+        <Col span={12}><Statistic title="Total Market Cap" className='statistic-title' value={millify(globalStats.totalMarketCap)}/></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" className='statistic-title' value={millify(globalStats.total24hVolume)}/></Col>
+        <Col span={12}><Statistic title="Total Markets" className='statistic-title' value={millify(globalStats.totalMarkets)}/></Col>
       </Row>
       <div className='home-heading-container'>
         <Title level={2} className='home-title'>Top 10 Cryptocurrencies in the world</Title>
